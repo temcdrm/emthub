4abfdb22196f69961ef17eb50327e4ff640d92be
temcdrm <t.mcdermott@ieee.org>
2024-10-15 16:31:41 -0400

    adding model info details

--- dll/gfm_gfl_ibr/test.c
+++ dll/gfm_gfl_ibr/test.c
@@ -20,6 +20,18 @@ DLL_MODEL_FCN LoadModelFunction (HINSTANCE hLib, char *name)
   return fcn;
 }
 
+const char *modeEMTorRMS (int val)
+{
+  if (val == 1) {
+    return "EMT";
+  } else if (val == 2) {
+    return "RMS";
+  } else if (val == 3) {
+    return "EMT+RMS";
+  }
+  return "n/a";
+}
+
 int main( void ) 
 { 
   HINSTANCE hLib; 
@@ -39,6 +51,26 @@ int main( void )
     if (NULL != Model_GetInfo) {
       const IEEE_Cigre_DLLInterface_Model_Info *pModelInfo = Model_GetInfo();
       printf("Model Name = %s\n", pModelInfo->ModelName);
+      printf("Model Version = %s\n", pModelInfo->ModelVersion);
+      printf("Updated %s by %s\n", pModelInfo->ModelLastModifiedDate, pModelInfo->ModelLastModifiedBy);
+      printf("Time Step: %0.5g [s]\n", pModelInfo->FixedStepBaseSampleTime);
+      printf("EMT/RMS Mode: %s\n", modeEMTorRMS (pModelInfo->EMT_RMS_Mode));
+      printf("Parameters/Description/Default:\n");
+      for (int k=0; k < pModelInfo->NumParameters; k++) {
+        printf("  %2d %-12s %-70s %-10s %g\n", k, pModelInfo->ParametersInfo[k].Name, pModelInfo->ParametersInfo[k].Description, 
+               pModelInfo->ParametersInfo[k].Unit, pModelInfo->ParametersInfo[k].DefaultValue.Real64_Val);
+      }
+      printf("Input Signals:\n");
+      for (int k=0; k < pModelInfo->NumInputPorts; k++) {
+        printf("  %2d %-12s %-70s %-10s\n", k, pModelInfo->InputPortsInfo[k].Name, pModelInfo->InputPortsInfo[k].Description, 
+               pModelInfo->InputPortsInfo[k].Unit);
+      }
+      printf("Output Signals:\n");
+      for (int k=0; k < pModelInfo->NumOutputPorts; k++) {
+        printf("  %2d %-12s %-70s %-10s\n", k, pModelInfo->OutputPortsInfo[k].Name, pModelInfo->OutputPortsInfo[k].Description, 
+               pModelInfo->OutputPortsInfo[k].Unit);
+      }
+      printf("Internal State Variables: %d int, %d float, %d double\n", pModelInfo->NumIntStates, pModelInfo->NumFloatStates, pModelInfo->NumDoubleStates);
     }
     Model_CheckParameters = LoadModelFunction (hLib, "Model_CheckParameters"); 
     Model_FirstCall = LoadModelFunction (hLib, "Model_FirstCall");
