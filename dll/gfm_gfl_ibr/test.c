5e3b7f7526309085f473a71f4f88ee4992e18ce8
temcdrm <t.mcdermott@ieee.org>
2024-10-15 14:42:29 -0400

    loading all the DLL functions

--- dll/gfm_gfl_ibr/test.c
+++ dll/gfm_gfl_ibr/test.c
@@ -1,24 +1,52 @@
-// From https://learn.microsoft.com/en-us/windows/win32/dlls/using-run-time-dynamic-linking
+// Copyright (C) 2024 Meltran, Inc
+
+// see https://learn.microsoft.com/en-us/windows/win32/dlls/using-run-time-dynamic-linking
  
 #include <windows.h> 
 #include <stdio.h> 
+
+#include "IEEE_Cigre_DLLInterface.h"
  
-typedef int (__cdecl *MYPROC)(void); 
- 
+typedef int32_T (__cdecl *DLL_INFO_FCN)(void); 
+typedef const IEEE_Cigre_DLLInterface_Model_Info * (__cdecl *DLL_STRUCT_FCN)(void);
+typedef int32_T (__cdecl *DLL_MODEL_FCN)(IEEE_Cigre_DLLInterface_Instance *);
+
+DLL_MODEL_FCN LoadModelFunction (HINSTANCE hLib, char *name)
+{
+  DLL_MODEL_FCN fcn = (DLL_MODEL_FCN) GetProcAddress (hLib, name);
+  if (NULL == fcn) {
+    printf("Failed to load %s\n", name);
+  }
+  return fcn;
+}
+
 int main( void ) 
 { 
-  HINSTANCE hinstLib; 
-  MYPROC ProcAdd; 
+  HINSTANCE hLib; 
+  DLL_INFO_FCN Model_PrintInfo;
+  DLL_STRUCT_FCN Model_GetInfo;
+  DLL_MODEL_FCN Model_CheckParameters, Model_Initialize, Model_Outputs, Model_FirstCall, Model_Iterate, Model_Terminate; 
   BOOL fFreeResult, fRunTimeLinkSuccess = FALSE;
  
-  hinstLib = LoadLibrary(TEXT("GFM_GFL_IBR.dll")); 
-  if (hinstLib != NULL) { 
-    ProcAdd = (MYPROC) GetProcAddress(hinstLib, "Model_PrintInfo"); 
-    if (NULL != ProcAdd) {
+  hLib = LoadLibrary(TEXT("GFM_GFL_IBR.dll")); 
+  if (hLib != NULL) { 
+    Model_PrintInfo = (DLL_INFO_FCN) GetProcAddress(hLib, "Model_PrintInfo"); 
+    if (NULL != Model_PrintInfo) {
       fRunTimeLinkSuccess = TRUE;
-      (ProcAdd) (); 
+//      Model_PrintInfo (); 
     }
-    fFreeResult = FreeLibrary(hinstLib); 
+    Model_GetInfo = (DLL_STRUCT_FCN) GetProcAddress(hLib, "Model_GetInfo");
+    if (NULL != Model_GetInfo) {
+      const IEEE_Cigre_DLLInterface_Model_Info *pModelInfo = Model_GetInfo();
+      printf("Model Name = %s\n", pModelInfo->ModelName);
+    }
+    Model_CheckParameters = LoadModelFunction (hLib, "Model_CheckParameters"); 
+    Model_FirstCall = LoadModelFunction (hLib, "Model_FirstCall");
+    Model_Initialize = LoadModelFunction (hLib, "Model_Initialize");
+    Model_Outputs = LoadModelFunction (hLib, "Model_Outputs");
+    Model_Iterate = LoadModelFunction (hLib, "Model_Iterate");
+    Model_Terminate = LoadModelFunction (hLib, "Model_Terminate");
+    fFreeResult = FreeLibrary(hLib); 
   } 
 
   if (!fRunTimeLinkSuccess) {
