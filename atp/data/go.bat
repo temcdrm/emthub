7d9a8884a397baa4cfc7dc42cc793b517075299a
temcdrm <t.mcdermott@ieee.org>
2025-09-25 14:41:25 -0400

    delay Q regulation by 1 second, remove wrong Q-block intial conditions in the IBR model

--- atp/data/go.bat
+++ atp/data/go.bat
@@ -1,8 +1,8 @@
-rem call runtp SyncMach.dbm > nul
-rem call runtp SyncMachCall.atp > nul
+rem call c:\atp\atpgnu\runtpgig SyncMach.dbm > nul
+rem call c:\atp\atpgnu\runtpgig SyncMachCall.atp > nul
 
-rem call runtp IBR.dbm > nul
-rem call runtp IBRCall.atp > nul
+rem call c:\atp\atpgnu\runtpgig IBR.dbm > nul
+rem call c:\atp\atpgnu\runtpgig IBRCall.atp > nul
 
 call c:\atp\atpgnu\runtpgig IEEE118.atp > nul
 rem call c:\atp\atpgnu\runtpgig WECC240.atp > nul
