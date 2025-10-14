93f0ef76f4cb46ff652d21d2c8eb839b890766b0
temcdrm <t.mcdermott@ieee.org>
2025-10-14 15:55:03 -0400

    initial DDL test

---
+++ cim/create_db.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2025 Meltran, Inc
+
+import sqlite3
+
+if __name__ == '__main__':
+  con = sqlite3.connect ('emtiop.db')
+  cur = con.cursor ()
+  with open ('emtiop.sql', 'r') as file:
+    script = file.read()
+  cur.executescript (script)
+  print(cur.fetchall())
+  con.close()
+
