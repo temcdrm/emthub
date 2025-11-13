8ce15e0cda44b7db83047aba7f943b305cb72394
temcdrm <t.mcdermott@ieee.org>
2025-11-13 12:35:42 -0500

    new DDL from CIMTool, but sqlite3 does not allow addition of foreign keys directly to an existing table

--- cim/create_db.py
+++ cim/create_db.py
@@ -5,7 +5,7 @@ import sqlite3
 if __name__ == '__main__':
   con = sqlite3.connect ('emtiop.db')
   cur = con.cursor ()
-  with open ('emtiop.sql', 'r') as file:
+  with open ('emtiop.sql', 'r', encoding='utf8') as file:
     script = file.read()
   cur.executescript (script)
   print(cur.fetchall())
