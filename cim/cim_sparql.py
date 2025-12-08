c7d49b301a788c7c398486a79c9e333c122c4920
temcdrm <t.mcdermott@ieee.org>
2025-12-08 12:59:46 -0500

    add ckt=2 flags to the second line connecting 42-49 and 49-66

--- cim/cim_sparql.py
+++ cim/cim_sparql.py
@@ -11,15 +11,19 @@ DELIM = ':'
 CASES = [
   {'id': '1783D2A8-1204-4781-A0B4-7A73A2FA6038', 
    'name': 'IEEE118', 
-   'rawfile':'raw/ieee-118-bus-v4.raw', 'cimfile':'ieee118.xml', 'locfile': 'raw/ieee118_network.json', 'mridfile':'raw/ieee118mrids.dat',
+   'rawfile':'raw/ieee-118-bus-v4.raw', 'xmlfile':'ieee118.xml', 'locfile': 'raw/ieee118_network.json', 'mridfile':'raw/ieee118mrids.dat', 'ttlfile': 'ieee118.ttl',
    'wind_units': ['132_W', '136_W', '138_W', '168_W', '180_W'],
    'solar_units': ['126_S', '128_S', '130_S', '140_S', '149_S', 
                    '151_S', '159_S', '165_S', '175_S', '179_S', 
                    '183_S', '185_S', '188_S', '191_S'],
-   'hydro_units': [], 'nuclear_units': []},
+   'hydro_units': [], 'nuclear_units': [],   
+   'bus_ic': 'c:/src/cimhub/bes/ieee118mb.txt',
+   'gen_ic': 'c:/src/cimhub/bes/ieee118mg.txt',
+   'swingbus':'131', 
+   'load': 0.6748},
   {'id': '2540AF5C-4F83-4C0F-9577-DEE8CC73BBB3', 
    'name': 'WECC240',
-   'rawfile':'raw/240busWECC_2018_PSS.raw', 'cimfile':'wecc240.xml', 'locfile': 'raw/wecc240_network.json', 'mridfile':'raw/wecc240mrids.dat',
+   'rawfile':'raw/240busWECC_2018_PSS.raw', 'xmlfile':'wecc240.xml', 'locfile': 'raw/wecc240_network.json', 'mridfile':'raw/wecc240mrids.dat', 'ttlfile': 'wecc240.ttl',
    'wind_units': ['1032_S', '1034_W', '1333_S', '2130_G', '2332_S', 
                   '2431_S', '2434_S', '2438_RG', '2438_SW', '2439_S'],
    'solar_units': ['2533_S', '2631_S', '3234_NW', '3433_S', '3835_NG', 
@@ -31,7 +35,11 @@ CASES = [
                    '4035_H', '4039_H', '4131_H', '4132_H', '4231_H', 
                    '5031_H', '6335_H', '6533_H', '7032_H', '8033_H', 
                    '8034_H'],
-   'nuclear_units': ['1431_N', '4132_N']}
+   'nuclear_units': ['1431_N', '4132_N'],
+   'bus_ic': 'c:/src/cimhub/bes/wecc240mb.txt',
+   'gen_ic': 'c:/src/cimhub/bes/wecc240mg.txt',
+   'swingbus':'2438', 
+   'load': 1.0425}
 ]
 
 def summarize_graph (g):
