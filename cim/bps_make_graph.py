0dfb97df482be5a050363082451285c5dde71265
temcdrm <t.mcdermott@ieee.org>
2025-12-10 13:05:17 -0500

    script to make network layouts

--- cim/bps_make_graph.py
+++ cim/bps_make_graph.py
@@ -1,13 +1,17 @@
 # Copyright (C) 2023 Battelle Memorial Institute
 # Copyright (C) 2025 Meltran, Inc
 
-import cimhub.api as cimhub
-import cimhub.CIMHubConfig as CIMHubConfig
 import sys
 import math
 import networkx as nx
 import json
 import os
+import rdflib
+import time
+import xml.etree.ElementTree as ET
+
+PREFIX = None
+DELIM = ':'
 
 CASES = [
   {'id': '1783D2A8-1204-4781-A0B4-7A73A2FA6038', 'name': 'IEEE118'},
@@ -18,6 +22,78 @@ CASES = [
 SQRT3 = math.sqrt(3.0)
 RAD_TO_DEG = 180.0 / math.pi
 MVA_BASE = 100.0
+
+def list_dict_table(dict, tag):
+  tbl = dict[tag]
+  print ('\n{:s}: key,{:s}'.format(tag, str(tbl['columns'])))
+  for key, row in tbl['vals'].items():
+    print ('{:s},{:s}'.format (key, ','.join(str(row[c]) for c in tbl['columns'])))
+
+def build_query (prefix, base, sysid):
+  if sysid is not None:
+    idx = base.find('WHERE {') + 8
+    return prefix + '\n' + base[:idx] + """ VALUES ?sysid {{"{:s}"^^c:String}}\n""".format (sysid) + base[idx:]
+  return prefix + '\n' + base
+
+def query_for_values (g, tbl, sysid):
+  keyflds = tbl['keyfld'].split(':')
+  q = build_query (PREFIX, tbl['sparql'], sysid)
+ # print ('===================')
+ # print (q)
+  result = g.query(q)
+  vars = [str(item) for item in result.vars]
+  for akey in keyflds:
+    vars.remove (akey)
+  tbl['columns'] = vars
+  for b in result:
+    row = {}
+    key = str(b[keyflds[0]])
+    for i in range(1, len(keyflds)):
+      key = key + DELIM + str(b[keyflds[i]])
+    for fld in vars:
+      if fld in ['name', 'conn', 'sysid', 'bus', 'bus1', 'bus2', 'id', 'eqid']:
+        row[fld] = str(b[fld])
+      else:
+        try:
+          row[fld] = int(b[fld])
+        except ValueError:
+          try:
+            row[fld] = float(b[fld])
+          except ValueError:
+            row[fld] = str(b[fld])
+    tbl['vals'][key] = row
+
+def load_emt_dict (g, xml_file, sysid):
+  global PREFIX
+  # read the queries into dict
+  tree = ET.parse(xml_file)
+  root = tree.getroot()
+  nsCIM = root.find('nsCIM').text.strip()
+  nsRDF = root.find('nsRDF').text.strip()
+  nsEMT = root.find('nsEMT').text.strip()
+  PREFIX = """PREFIX r: <{:s}>\nPREFIX c: <{:s}>\nPREFIX e: <{:s}>""".format (nsRDF, nsCIM, nsEMT)
+  #print (PREFIX)
+  dict = {}
+  for query in root.findall('query'):
+    qid = query.find('id').text.strip()
+    dict[qid] = {}
+    dict[qid]['keyfld'] = query.find('keyfld').text
+    dict[qid]['sparql'] = query.find('value').text.strip()
+    dict[qid]['columns'] = []
+    dict[qid]['vals'] = {}
+    #print (' ', qid, dict[qid]['keyfld'])
+
+  for key in ['EMTContainer', 'EMTBus', 'EMTBusXY', 'EMTBaseVoltage', 'EMTLine', 'EMTLoad',
+              'EMTCountPowerXfmrWindings', 'EMTPowerXfmrWinding', 'EMTPowerXfmrCore',
+              'EMTPowerXfmrMesh', 'EMTXfmrSaturation', 'EMTCompShunt', 'EMTCompSeries',
+              'EMTSyncMachine', 'EMTSolar', 'EMTWind', 'EMTGovSteamSGO', 'EMTExcST1A',
+              'EMTPssIEEE1A', 'EMTWeccREGCA', 'EMTWeccREECA', 'EMTWeccREPCA',
+              'EMTWeccWTGTA', 'EMTWeccWTGARA', 'EMTEnergySource', 'EMTDisconnectingCircuitBreaker']:
+    start_time = time.time()
+    query_for_values (g, dict[key], sysid)
+    print ('  Running {:40s} took {:6.3f} s for {:5d} rows'.format (key, time.time() - start_time, len(dict[key]['vals'])))
+ #   list_dict_table (dict, key)
+  return dict
 
 def km_distance (G, n1, n2):
 #  seq = nx.dijkstra_path(G, n1, n2)
@@ -32,14 +108,14 @@ def km_distance (G, n1, n2):
 
 def build_system_graph (d):
   # accumulate loads and generation onto the buses
-  buses = d['BESBus']['vals']
-  for key, data in d['BESLoad']['vals'].items():
+  buses = d['EMTBus']['vals']
+  for key, data in d['EMTLoad']['vals'].items():
     buses[data['bus']]['has_load'] = True
-  for key, data in d['BESMachine']['vals'].items():
+  for key, data in d['EMTSyncMachine']['vals'].items():
     buses[data['bus']]['has_gen'] = True
-  for key, data in d['BESSolar']['vals'].items():
+  for key, data in d['EMTSolar']['vals'].items():
     buses[data['bus']]['has_solar'] = True
-  for key, data in d['BESWind']['vals'].items():
+  for key, data in d['EMTWind']['vals'].items():
     buses[data['bus']]['has_wind'] = True
   G = nx.Graph()
   for key, data in buses.items():
@@ -56,7 +132,7 @@ def build_system_graph (d):
     G.add_node (key, nclass=nclass, ndata={'kv':0.001*data['nomv']})
   # accumulate the transformer windings into transformers
   xfmrs = {}
-  for key, data in d['BESPowerXfmrWinding']['vals'].items():
+  for key, data in d['EMTPowerXfmrWinding']['vals'].items():
     toks = key.split(':')
     pname = toks[0]
     busnum = 'bus{:s}'.format(toks[1])
@@ -66,14 +142,14 @@ def build_system_graph (d):
       xfmrs[pname][busnum] = data['bus']
 
   # add line, transformer, series compensator branches
-  for key, data in d['BESLine']['vals'].items():
+  for key, data in d['EMTLine']['vals'].items():
     km = round(0.001*data['len'],3)
     G.add_edge(data['bus1'],data['bus2'],eclass='line',ename=key,
                edata={'km':km, 'kv':0.001*data['basev']}, weight=km)
   for key, data in xfmrs.items():
     G.add_edge(data['bus1'],data['bus2'],eclass='transformer',ename=key,
                edata={'km':1.0}, weight=1.0)
-  for key, data in d['BESCompSeries']['vals'].items():
+  for key, data in d['EMTCompSeries']['vals'].items():
     G.add_edge(data['bus1'],data['bus2'],eclass='series',ename=key,
                edata={'km':1.0, 'kv':0.001*data['basev']}, weight=1.0)
 
@@ -101,19 +177,26 @@ def save_system_graph (G, fname):
   fp.close()
 
 if __name__ == '__main__':
-  CIMHubConfig.ConfigFromJsonFile ('cimhubconfig.json')
   case_id = 0
   if len(sys.argv) > 1:
     case_id = int(sys.argv[1])
-  sys_id = CASES[case_id]['id']
-  sys_name = CASES[case_id]['name']
+  case = CASES[case_id]
+  sys_id = case['id']
+  sys_name = case['name']
 
-  d = cimhub.load_bes_dict ('qbes.xml', sys_id, bTime=False)
-#  cimhub.summarize_bes_dict (d)
-#  cimhub.list_dict_table (d, 'BESContainer')
-#  cimhub.list_dict_table (d, 'BESBaseVoltage')
+  g = rdflib.Graph()
+  fname = sys_name + '.ttl'
+  g.parse (fname)
+  print ('read', len(g), 'statements from', fname)
+  #summarize_graph (g)
+
+  start_time = time.time()
+  d = load_emt_dict (g, 'sparql_queries.xml', case['id'])
+  print ('Total query time {:6.3f} s'.format (time.time() - start_time))
+  #list_dict_table (d, 'EMTBus')
+  #quit()
 
   G = build_system_graph (d)
-  save_system_graph (G, '{:s}_Network.json'.format(sys_name))
+  save_system_graph (G, './raw/{:s}_Network.json'.format(sys_name))
 
 
