9b71173a18dfdf732340a4c67c0b7250b4619cbd
temcdrm <t.mcdermott@ieee.org>
2025-12-11 13:59:19 -0500

    IEEE39 runs in ATP

--- atp/data/convert.bat
+++ atp/data/convert.bat
@@ -12,7 +12,11 @@ rem python ..\src\plot_pd5.py WECC240.hdf5
 rem python ..\src\plot118.py
 rem python ..\src\plot240.py
 
-python ..\src\pl4_to_pd5.py XfmrSat
-python ..\src\plot_pd5.py XfmrSat.hdf5
-python ..\src\plotXfmrSat.py
+rem python ..\src\pl4_to_pd5.py XfmrSat
+rem python ..\src\plot_pd5.py XfmrSat.hdf5
+rem python ..\src\plotXfmrSat.py
+
+rem python ..\src\pl4_to_pd5.py IEEE39
+rem python ..\src\plot_pd5.py ieee39.hdf5
+python ..\src\plot39.py
 
--- atp/data/go.bat
+++ atp/data/go.bat
@@ -6,7 +6,8 @@ rem call c:\atp\atpgnu\runtpgig IBRCall.atp > nul
 
 rem call c:\atp\atpgnu\runtpgig IEEE118.atp > nul
 rem call c:\atp\atpgnu\runtpgig WECC240.atp > nul
+call c:\atp\atpgnu\runtpgig IEEE39.atp > nul
 
-call c:\atp\atpgnu\runtpgig XfmrSat.atp > nul
+rem call c:\atp\atpgnu\runtpgig XfmrSat.atp > nul
 
 
