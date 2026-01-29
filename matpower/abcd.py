3354e431c8ad99c862a96a33f552005a3720b7bf
temcdrm <t.mcdermott@ieee.org>
2026-01-29 14:38:43 -0500

    corrected ABCD parameters and use nominal PI for MATPOWER, long-line for ATP comparison

--- matpower/abcd.py
+++ matpower/abcd.py
@@ -3,21 +3,34 @@
 
 import math
 import cmath
-LONGLINE = True
+LONGLINE = False
+USE_ATP_SOLN = False
+SQRT3 = math.sqrt(3.0)
 
 if __name__ == '__main__':
   # ACLineSegment attributes for the total length
   Z = complex(20.0, 188.5)
   Y = complex(0.0, 0.002092)
-  # known variables at the "from" bus, port 2 in the ABCD network
-  s2 = complex(341453311.0, 7543467.0)
-  v2 = complex(345000.0, 0.0)
-  i2 = (-s2/v2).conjugate() # i2 defined to flow out of port 2
-  # known voltage and power at the "to" bus
-  v1solved = cmath.rect (309915.22500000003, math.radians(-35.25937))
-  s1solved = complex(-318932949.0, -20252242.0)
 
-  print ('v2=', cmath.polar(v2))
+  if USE_ATP_SOLN:
+    s2 = complex(353330000.0,-3590000.0)
+    v2ln = complex(344888.3/SQRT3, math.radians(-0.202722)) # this is now line-to-neutral
+    i2 = (-s2/v2ln/3.0).conjugate() # i2 defined to flow out of port 2
+    # known voltage and power at the "to" bus
+    v1solved = cmath.rect (315781.75, math.radians(-33.69307))
+    s1solved = complex(-331880000.0, -25220000.0)
+    print ('** Using ATP Phasor Solution')
+  else:
+    # known variables at the "from" bus, port 2 in the ABCD network
+    s2 = complex(341453311.0, 7543467.0)
+    v2ln = complex(345000.0/SQRT3, 0.0) # this is now line-to-neutral
+    i2 = (-s2/v2ln/3.0).conjugate() # i2 defined to flow out of port 2
+    # known voltage and power at the "to" bus
+    v1solved = cmath.rect (309915.22500000003, math.radians(-35.25937))
+    s1solved = complex(-318932949.0, -20252242.0)
+    print ('** Using MATPOWER Solution')
+
+  print ('v2ln=', cmath.polar(v2ln))
   print ('i2=', cmath.polar(i2))
 
   # line parameters
@@ -32,14 +45,15 @@ if __name__ == '__main__':
   else:
     A = 1 + 0.5*Y*Z
     D = A
-    B = Z * (1.0 + Y*Z/6.0)
-    C = Y * (1.0 + Y*Z/6.0)
+    B = Z
+    C = Y * (1.0 + Y*Z/4.0)
     print (' (using nominal pi section)')
 
   # calculate the port 1 parameters
-  v1 = A*v2 + B*i2
-  i1 = C*v2 + D*i2
-  s1 = v1*i1.conjugate()
+  v1ln = A*v2ln + B*i2
+  i1 = C*v2ln + D*i2
+  s1 = 3.0*v1ln*i1.conjugate()
+  v1 = SQRT3*v1ln
 
   print ('gammaL=', gammaL)
   print ('Zc=', Zc)
@@ -52,13 +66,18 @@ if __name__ == '__main__':
   print ('i1=', cmath.polar(i1))
   print ('s1=', s1, 'compared to', s1solved)
 
+  print ('ERROR Summary:')
+  print ('  v1mag = {:.4f} [%]'.format (100.0*(abs(v1) - abs(v1solved))/abs(v1solved)))
+  print ('     P1 = {:.4f} [%]'.format (100.0*(abs(s1.real) - abs(s1solved.real))/abs(s1solved.real)))
+  print ('     Q1 = {:.4f} [%]'.format (100.0*(abs(s1.imag) - abs(s1solved.imag))/abs(s1solved.imag)))
+
   # find currents and losses in the pi branches
-  Iseries = (v2 - v1) / Z
-  Ishunt2 = v2 * Y / 2.0
-  Ishunt1 = v1 * Y / 2.0
-  Ploss = abs(Iseries)**2 * Z.real
+  Iseries = (v2ln - v1ln) / Z
+  Ishunt2 = v2ln * Y / 2.0
+  Ishunt1 = v1ln * Y / 2.0
+  Ploss = 3*(abs(Iseries)**2 * Z.real)
   print ('Pi P loss=', Ploss*1.0e-6, 'compared to ABCD', (s2.real + s1.real)*1.0e-6, 'or MATPOWER', (s2.real + s1solved.real)*1.0e-6)
-  Qloss = abs(Iseries)**2 * Z.imag - 0.5 * abs(v1)**2 * Y.imag - 0.5 * abs(v2)**2 * Y.imag
+  Qloss = 3*(abs(Iseries)**2 * Z.imag - 0.5 * abs(v1ln)**2 * Y.imag - 0.5 * abs(v2ln)**2 * Y.imag)
   print ('Pi Q loss=', Qloss*1.0e-6, 'compared to ABCD', (s2.imag + s1.imag)*1.0e-6, 'or MATPOWER', (s2.imag + s1solved.imag)*1.0e-6)
 
   # final answer
