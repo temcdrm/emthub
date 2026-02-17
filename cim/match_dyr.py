fd8420654c3ba5f96658a11e8e57ab8db6fe7c36
temcdrm <t.mcdermott@ieee.org>
2026-02-17 15:23:04 -0500

    dyr file class and attribute mapping for the 3 IBR network model examples

--- cim/match_dyr.py
+++ cim/match_dyr.py
@@ -24,12 +24,19 @@ if __name__ == '__main__':
   g.parse('../emtiop/emtiop.owl')
   summarize_graph (g)
 
+  md = {}
   with open('dyr_config.json', 'r', encoding='utf-8-sig') as file:
     dyr = json.load (file)
   print ('PSSE       CIM                                      #dyr')
   for key, row in dyr.items():
     CIMclass = row['CIMclass']
-    if len(CIMclass) > 0:
+    if CIMclass is not None and len(CIMclass) > 0:
+      md[key] = {'CIMclass': CIMclass, 'parameters_after_ID': []}
       print ('{:10s} {:40s} {:4d}'.format (key, row['CIMclass'], len(row['inputs'])))
-
+      vals = row['inputs']
+      for i in range(len(vals)):
+        if vals[i] != 'BUS' and vals[i] != 'ID':
+          md[key]['parameters_after_ID'].append ({'dyr': vals[i], 'cim': '{:s}.{:s}'.format (CIMclass, vals[i].lower())})
+  with open ('dyr_mapping.json', 'w') as file:
+    json.dump (md, file, indent=2)
 
