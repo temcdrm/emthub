925aff5aa85866536b2a81e7f10a4325c28a4f18
temcdrm <t.mcdermott@ieee.org>
2026-06-04 00:49:57 -0400

    restoring the Terminals to matpower_write_ic

--- src/emthub/examples/matpower_write_ic.m
+++ src/emthub/examples/matpower_write_ic.m
@@ -11,24 +11,19 @@ function write_ic = matpower_write_ic (mpc, fbus, fgen, fbranch)
     fclose(fid);
 
     fid=fopen(fgen, "w");
-    fprintf (fid, "Bus, Pg, Qg, Type, CEQ id\n");
+    fprintf (fid, "Bus, Pg, Qg, Type, CEQ t1_id\n");
     n=size(mpc.gen)(1);
     for i=1:n
-        fprintf (fid, "%d,%12.6f,%12.6f,%s,%s\n", mpc.gen(i,GEN_BUS), mpc.gen(i,PG), mpc.gen(i,QG), mpc.gentype{i}, mpc.gen_id{i});
+        fprintf (fid, "%d,%12.6f,%12.6f,%s,%s\n", mpc.gen(i,GEN_BUS), mpc.gen(i,PG), mpc.gen(i,QG), mpc.gentype{i}, mpc.gen_t1_id{i});
     endfor
     fclose(fid);
 
     fid=fopen(fbranch, "w");
-    fprintf (fid, "From, To, Ratio, Pfrom, Qfrom, Pto, Qto, CEQ/XfEnd id\n");
+    fprintf (fid, "From, To, Ratio, Pfrom, Qfrom, Pto, Qto, t1_id, t2_id\n");
     n=size(mpc.branch)(1);
     for i=1:n
         tap = mpc.branch(i,TAP);
-        if (tap > 0.0)
-            fprintf (fid, "%d,%d,%6.4f,%12.6f,%12.6f,%12.6f,%12.6f,%s\n", mpc.branch(i,F_BUS), mpc.branch(i,T_BUS), tap, mpc.branch(i,PF), mpc.branch(i,QF), mpc.branch(i,PT), mpc.branch(i,QT), mpc.branch_id{i});
-            fprintf (fid, "%d,%d,%6.4f,%12.6f,%12.6f,%12.6f,%12.6f,%s\n", mpc.branch(i,T_BUS), mpc.branch(i,F_BUS), tap, mpc.branch(i,PT), mpc.branch(i,QT), mpc.branch(i,PF), mpc.branch(i,QF), mpc.xfsec_id{i});
-        else
-            fprintf (fid, "%d,%d,%6.4f,%12.6f,%12.6f,%12.6f,%12.6f,%s\n", mpc.branch(i,F_BUS), mpc.branch(i,T_BUS), tap, mpc.branch(i,PF), mpc.branch(i,QF), mpc.branch(i,PT), mpc.branch(i,QT), mpc.branch_id{i});
-        endif
+        fprintf (fid, "%d,%d,%6.4f,%12.6f,%12.6f,%12.6f,%12.6f,%s,%s\n", mpc.branch(i,F_BUS), mpc.branch(i,T_BUS), tap, mpc.branch(i,PF), mpc.branch(i,QF), mpc.branch(i,PT), mpc.branch(i,QT), mpc.branch_t1_id{i}, mpc.branch_t2_id{i});
     endfor
     fclose(fid);
 endfunction
