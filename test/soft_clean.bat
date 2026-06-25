06f6e70ea4c7f8dc90d5402ae2ca2342f21fda65
temcdrm <t.mcdermott@ieee.org>
2026-06-24 23:58:08 -0400

    clean up temporary files but leave the inputs and solutions in place

---
+++ test/soft_clean.bat
@@ -0,0 +1,23 @@
+@echo off
+rem (skip) cleaning up input files for CIM RDF
+
+rem (skip) clean up my local scripts
+
+rem (soft) clean up after MATPOWER
+del solve*.m 2>nul
+del *Solved.m 2>nul
+git checkout *summary.txt 2>nul
+
+rem (soft) clean up after ATP, leave the inputs and final outputs
+del *.lis 2>nul
+del *.dbg 2>nul
+del fort.* 2>nul
+del *.pl4 2>nul
+del commands.script 2>nul
+del *.log 2>nul
+del *.tmp 2>nul
+del *.bin 2>nul
+del *.hdf5 2>nul
+del *.dat 2>nul
+del *.cfg 2>nul
+del *.png 2>nul
